26479445ab5c6229f68dd6881afa1f644e154f2f
Foreygon <111753615+Foreygon@users.noreply.github.com>
2022-09-27 14:21:13 +0200

    4ieme commit

---
+++ index.js
@@ -0,0 +1,28 @@
+"use strict";
+
+const express = require('express');
+const app = express();
+
+
+
+// import
+const guessPort = require('./utils/port')
+const port = guessPort();
+
+console.log( port );
+
+
+// app settings
+// --
+
+
+// app setup
+// --
+
+
+// routing
+// --
+
+
+// start server app
+// --
---
+++ utils/port.js
@@ -0,0 +1,21 @@
+'use strict';
+
+
+const net =require('net');
+
+module.exports = function() 
+{
+//creation du serveur avec le module "net"
+    const server = net.createServer();
+// demarrage du server sur le port "0"
+// port 0 exist pas le module net va rechercher un port dispo
+    server.listen(0);
+// recup du num de port attribué par net
+    let port = server.address().port;
+// ferme le serveur
+    server.close;
+// retourne le numero de port
+    return port;
+
+
+}
